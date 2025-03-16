020611dab921a27f2abb87944303b7e44e531867
Gustavo Perdomo <gperdomor@gmail.com>
2025-03-16 19:23:20 -0300

    feat(website): added initial docs

--- apps/website/src/lib/source.ts
+++ apps/website/src/lib/source.ts
@@ -7,11 +7,9 @@ export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
   icon(icon) {
-    if (!icon) {
-      // You may set a default icon
-      return;
+    if (icon && icon in icons) {
+      return createElement(icons[icon as keyof typeof icons]);
     }
-
-    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+    return undefined;
   },
 });
