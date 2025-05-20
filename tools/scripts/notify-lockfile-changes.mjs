5614f6811a5df2fc970cb4c2225eeb9e333835ff
Gustavo Perdomo <gperdomor@gmail.com>
2025-05-20 17:19:59 -0400

    chore(source): improved commitlint config and validation scripts

--- tools/scripts/notify-lockfile-changes.mjs
+++ tools/scripts/notify-lockfile-changes.mjs
@@ -1,9 +1,5 @@
+import { consola } from 'consola';
+
 if (process.argv.slice(2).some((arg) => arg.includes('package-lock.json'))) {
-  console.warn(
-    [
-      '⚠️ ------------------------------------------------------------------------------------------- ⚠️',
-      '⚠️ package-lock.json changed, please run `npm install` to ensure your packages are up to date. ⚠️',
-      '⚠️ ------------------------------------------------------------------------------------------- ⚠️',
-    ].join('\n')
-  );
+  consola.warn('⚠️  package-lock.json changed, please run `npm install` to ensure your packages are up to date. ⚠️');
 }
